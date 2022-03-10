87dcf8e89bfce25cdc0c81a5ec274624daa659ab
Oguz <oguz.oeztuerk@outlook.de>
2022-03-10 22:34:35 +0100

    remove body-parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
@@ -9,8 +8,8 @@ import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
